6a11851606552eb996e9e50419d73ccb5f5b8377
aakhazov <aleks.khazow@gmail.com>
2023-06-14 18:27:29 +0300

    Add index for clients table.

--- src/main/resources/db/changelog/1.0.0/table/08-06-23-create_table-clients.sql
+++ src/main/resources/db/changelog/1.0.0/table/08-06-23-create_table-clients.sql
@@ -20,3 +20,10 @@ CREATE TABLE IF NOT EXISTS clients_schema.clients
     version              INTEGER,
     CONSTRAINT pk_clients PRIMARY KEY (id)
 );
+
+--changeset akhazov:14-06-23-create_index-for-table-clients
+CREATE INDEX idx_clients_first_name ON clients_schema.clients (first_name);
+CREATE INDEX idx_clients_middle_name ON clients_schema.clients (middle_name);
+CREATE INDEX idx_clients_last_name ON clients_schema.clients (last_name);
+CREATE INDEX idx_clients_email ON clients_schema.clients (email);
+CREATE INDEX idx_clients_phone_number ON clients_schema.clients (phone_number);
